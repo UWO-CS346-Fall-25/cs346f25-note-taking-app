e802ae9e0531961f5a4a2aafcb3572786eda8b8b
Matthew Sabel <matthew.sabel@charter.net>
2025-10-19 12:11:45 -0500

    Feature/week7 layout (#1)
    * Added navigation to About and Register page
    
    * Matt working on ABOUT, James working on REGISTER
    
    * Register page finished, About page finished, index/homepage styled, image folder created
    
    * James: made changes to the style.css
    
    * Added better logo to replace WebNote title in top left of all pages, finished login screen, added favicon to all pages, labeled all pages with proper title. Changed background to gradient for About page.
    
    * Miscellaneous changes
    
    ---------
    
    Co-authored-by: Thaoja24 <thaoja24@uwosh.edu>

--- src/app.js
+++ src/app.js
@@ -81,7 +81,31 @@ app.get('/', csrfProtection, (req, res) => {
   });
 });
 
-// 404 handler
+//Register page
+app.get('/users/register', csrfProtection, (req, res) => {
+  res.render('register', {
+    title: 'Register',
+    csrfToken: req.csrfToken(),
+  });
+});
+
+//About page
+app.get('/users/about', csrfProtection, (req, res) => {
+  res.render('about', {
+    title: 'About',
+    csrfToken: req.csrfToken(),
+  });
+});
+
+// Log in page
+app.get('/users/login', csrfProtection, (req, res) => {
+  res.render('login', {
+    title: 'Login',
+    csrfToken: req.csrfToken(),
+  });
+});
+
+// 404 handler error page
 app.use((req, res) => {
   res.status(404).render('error', {
     title: 'Page Not Found',
